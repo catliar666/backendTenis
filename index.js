22bb1072939086f428fa31bc4604f5f2628c34d6
Maria Ordóñez <maariiaa1912@gmail.com>
2024-12-16 18:21:13 +0100

    implementacion cors

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors')
+const cors = require('cors')
 app.use(express.json())
 const routes = require('./v1/routes/tenis_rutas')
 
 
-// app.use(cors())
+app.use(cors())
 const PORT = process.env.PORT || 1234;
 
 app.use('/api/v1/partidos', routes)
